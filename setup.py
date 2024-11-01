7fd5edc7065afeec2c1d5539e248aaa26d691b3f
cdhainaut <ch.dhainaut@gmail.com>
2024-11-01 18:57:33 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import os
+
+# Define Cython extensions
+extensions = [Extension("greety.example", ["greety/example.py"])]
+
+setup(
+    name="greety",
+    version="0.1.0",
+    author="Charles Dhainaut",
+    author_email="ch.dhainaut@gmail.com",
+    description="A sample package compiled with Cython",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=["greety"],
+    ext_modules=cythonize(extensions, compiler_directives={"language_level": "3"}),
+    zip_safe=False,
+)
